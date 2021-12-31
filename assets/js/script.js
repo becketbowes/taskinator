f6733649091555514de90aae09a6799c82a251a8
becket <becket.bowes@gmail.com>
2021-12-30 20:51:42 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,34 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  //create list item
+  //pkg data as object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  //send object as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
+    //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
   //create div for task, give it class, give it content
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-  
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
+  
   //add entire enchillada to the list
   tasksToDoEl.appendChild(listItemEl);
-  console.dir(listItemEl);
-};
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
