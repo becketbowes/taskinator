15de306d176780a8ae95fe8bc1f164d2f719057e
becket <becket.bowes@gmail.com>
2021-12-30 20:34:15 -0500

    user info now goes into task items

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  //create div for task, give it class, give it content
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+  listItemEl.appendChild(taskInfoEl);
+  //add entire enchillada to the list
   tasksToDoEl.appendChild(listItemEl);
-  console.log(event);
+  console.dir(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
